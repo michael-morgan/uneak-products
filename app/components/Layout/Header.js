3853b6c3c3df0974df8b4feb89ed5743a9e79311
Michael Morgan <personal.michaelmorgan@gmail.com>
2017-04-15 10:45:35 -0400

    Removed React Static Boilerplate labelling.

--- app/components/Layout/Header.js
+++ app/components/Layout/Header.js
@@ -1,13 +1,3 @@
-/**
- * React Static Boilerplate
- * https://github.com/kriasoft/react-static-boilerplate
- *
- * Copyright © 2015-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import Navigation from './Navigation';
 import Link from '../Link';
@@ -28,7 +18,7 @@ class Header extends React.Component {
       <header className={`mdl-layout__header ${s.header}`} ref={node => (this.root = node)}>
         <div className={`mdl-layout__header-row ${s.row}`}>
           <Link className={`mdl-layout-title ${s.title}`} to="/">
-            React Static Boilerplate
+            Uneak Products
           </Link>
           <div className="mdl-layout-spacer"></div>
           <Navigation />
